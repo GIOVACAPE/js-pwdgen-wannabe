fb962685dea9be01333149540fdebf0eb1f2a826
Giovanni Capelli <giovacape01@gmail.com>
2022-12-19 10:06:15 +0100

    Completato.

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-alert('Buongiorno Utente');
+//alert('Buongiorno Utente');
 
 // INIZIO --------------------------
 
@@ -6,28 +6,34 @@ alert('Buongiorno Utente');
 // 1. Chiedi all'utente il suo nome
 
 const Nome = prompt('qual è il tuo nome?');
-console.log(typeof Nome)
+console.log(Nome);
+console.log(typeof Nome);
 
 
 
 //2. chiedi il suo cognome
 const Cognome = prompt('qual è il tuo cognome?');
-console.log(typeof Cognome)
+console.log(Cognome);
+console.log(typeof Cognome);
 
 
 // 3. chiedi il suo colore preferito
 const ColorePreferito = prompt('qual è il tuo colore preferito?');
-console.log(typeof ColorePreferito)
+console.log(ColorePreferito);
+console.log(typeof ColorePreferito);
 
 
 
 
 
 // 4. Infine scrivi sulla pagina nomecognomecolorepreferito21
+let password = Nome + Cognome + ColorePreferito + '21';
+console.log(password);
+console.log(typeof password);
 
+//riempimento
 
-
-
+document.getElementById('info').innerHTML = password;
 
 
 // FINE ------------------------------------------

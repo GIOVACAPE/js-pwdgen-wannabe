e7702d457377bfaf58cc93b1a37d719e2d903501
Giovanni Capelli <giovacape01@gmail.com>
2022-12-19 09:41:20 +0100

    repo settata.

---
+++ js/script.js
@@ -0,0 +1,29 @@
+console.log('Init ok');
+
+// INIZIO --------------------------
+
+
+
+
+// 1. Chiedi all'utente il suo nome
+
+
+
+//2. chiedi il suo cognome
+
+
+
+// 3. chiedi il suo colore preferito
+
+
+
+
+
+// 4. Infine scrivi sulla pagina nomecognomecolorepreferito21
+
+
+
+
+
+
+// FINE ------------------------------------------
